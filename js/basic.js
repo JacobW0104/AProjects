4dd57931abe333d95eefe22aae089608708b4d8b
JacobW0104 <126601871+JacobW0104@users.noreply.github.com>
2023-04-25 14:31:05 +0100

    Add files via upload

---
+++ js/basic.js
@@ -0,0 +1,9 @@
+function open2(title){
+    let popup = document.getElementById(title);
+    popup.classList.add("open-popup");
+}
+
+function close2(title){
+    let popup = document.getElementById(title);
+    popup.classList.remove("open-popup");
+}
